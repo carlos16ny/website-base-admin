58652aee14cc4bb70b93c0bcb2af4a07b80700d2
Carlos Abreu <carlosabreu@192.168.0.124>
2019-07-24 17:06:19 -0300

    teste heroku

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-   phpinfo();
+   // phpinfo();
    /**Fazer o autoload das configurações e load das informações */
 
-   // require_once 'vendor/autoload.php';
-   // require_once 'load.php';
+   require_once 'vendor/autoload.php';
+   require_once 'load.php';
 
    /**
     * Incluir áreas do site aqui
@@ -11,17 +11,17 @@
     */
 
     /** include template site [header] */
-   // include_once 'src/template/header.php';
+   include_once 'src/template/header.php';
 
    /**
     * Incluir as sections do site
     */
-   // include_once 'src/sections/sec1.php';
-   // include_once 'src/sections/sec2.php';
-   // include_once 'src/sections/sec3.php';
+   include_once 'src/sections/sec1.php';
+   include_once 'src/sections/sec2.php';
+   include_once 'src/sections/sec3.php';
 
     /** include template site [footer] */
-   // include_once 'src/template/footer.php';
+   include_once 'src/template/footer.php';
 
 
 
